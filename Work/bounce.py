cd6a4bf65f25a5a024ecb9d7176f8a34af8e3d04
Matt Davis <matt@da5is.com>
2021-02-25 22:44:39 -0600

    few exercises

--- Work/bounce.py
+++ Work/bounce.py
@@ -1,3 +1,13 @@
 # bounce.py
 #
 # Exercise 1.5
+# A rubber ball is dropped from a height of 100 meters and each time it hits the ground, it bounces back up to 3/5 the height it fell. Write a program bounce.py that prints a table showing the height of the first 10 bounces.
+
+height = 100.00
+bounce_distance = 3/5
+times_bounced = 1
+
+while times_bounced <= 10:
+    height = height * bounce_distance
+    print (f'{times_bounced} {round(height, 4)}')
+    times_bounced += 1
--- Work/mortgage.py
+++ Work/mortgage.py
@@ -1,3 +1,27 @@
 # mortgage.py
 #
 # Exercise 1.7
+
+
+principal = 500000.0
+rate = 0.05
+payment = 2684.11
+total_paid = 0.0
+extra_payment_start_month = 61
+extra_payment_end_month = 108
+extra_payment = 1000
+current_month = 1
+
+while principal > 0:
+    if extra_payment_start_month <= current_month <= extra_payment_end_month:
+        total_paid = total_paid + extra_payment
+        principal = principal - extra_payment
+    principal = principal * (1+rate/12) - payment
+    total_paid = total_paid + payment
+    if principal < 0:
+        total_paid = total_paid + principal
+        principal = 0
+    print(f'{current_month}: {round(principal,2):.2f} {round(total_paid,2):.2f}')
+    current_month = current_month + 1
+
+print('Total paid', total_paid)
