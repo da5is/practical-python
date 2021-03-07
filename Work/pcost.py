16c38f88fe238bbcdf056d1d76620cb85f4fc878
Matt Davis <matt@da5is.com>
2021-03-07 09:34:52 -0600

    March 7, 2021 9:34 AM

--- Work/pcost.py
+++ Work/pcost.py
@@ -1,6 +1,8 @@
 # pcost.py
 #
 # Exercise 1.27
+import sys
+
 
 def portfolio_cost(filename):
     import csv
@@ -16,6 +18,10 @@ def portfolio_cost(filename):
             portfolio_cost += shares*price
     return(portfolio_cost)
 
-cost = portfolio_cost('Data\portfolio.csv')
-cost = portfolio_cost('Data\missing.csv')
+if len(sys.argv) == 2:
+    filename = sys.argv[1]
+else:
+    filename = 'Data\portfolio.csv'
+cost = portfolio_cost(filename)
+
 print(cost)
