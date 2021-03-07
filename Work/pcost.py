dcf9a585ab8a0ba5def24683e1c3e505ddc81edb
Matt Davis <matt@da5is.com>
2021-03-07 09:19:52 -0600

    March 7, 2021 9:19 AM

--- Work/pcost.py
+++ Work/pcost.py
@@ -1,3 +1,15 @@
 # pcost.py
 #
 # Exercise 1.27
+total_cost = 0.0
+
+with open('Data\portfolio.csv', 'rt') as f:
+    line = next(f)
+    for line in f:
+        print(line)
+        name = str(line[0])
+        shares = int(line[1])
+        price = float(line[2])
+        total_cost += shares * price
+
+print(total_cost)
